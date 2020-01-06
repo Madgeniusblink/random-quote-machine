3dbd7347746a4787fafd9e7ad068684c98034484
Madgeniusblink <madgeniusblink@gmail.com>
2020-01-06 13:00:48 -0700

    Updates

---
+++ precache-manifest.46fed2c871fb4ea189ae7cb5a51317ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a220fddf88e655606a585f88fa4903e",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5fc9fc96c46745856993",
+    "url": "/random-quote-machine/static/css/main.b30e35bf.chunk.css"
+  },
+  {
+    "revision": "c6beaeb98a4dd78c091f",
+    "url": "/random-quote-machine/static/js/2.c15a8e4c.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/random-quote-machine/static/js/2.c15a8e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fc9fc96c46745856993",
+    "url": "/random-quote-machine/static/js/main.72a4a932.chunk.js"
+  },
+  {
+    "revision": "936c152fa8bd8fb1282a",
+    "url": "/random-quote-machine/static/js/runtime-main.19882ef4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-quote-machine/precache-manifest.fe415252a1e759f4679ffc40ef36dedb.js"
+  "/random-quote-machine/precache-manifest.46fed2c871fb4ea189ae7cb5a51317ae.js"
 );
 
 self.addEventListener('message', (event) => {
